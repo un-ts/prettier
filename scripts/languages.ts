e93170adc13194821e2178249d52e488ffce12c4
JounQin <admin@1stg.me>
2022-07-29 21:40:11 +0800

    build: languages from package `linguist-languages` instead

--- scripts/languages.ts
+++ scripts/languages.ts
@@ -1,30 +1,7 @@
 import fs from 'node:fs'
-import { get } from 'node:https'
 
-import createHttpsProxyAgent from 'https-proxy-agent'
-import { load } from 'js-yaml'
-import _ from 'lodash'
+import LinguistLanguages from 'linguist-languages'
 import { SupportLanguage } from 'prettier'
-
-const proxyUrl =
-  process.env.https_proxy || process.env.http_proxy || process.env.all_proxy
-
-const linguistLanguages =
-  'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml'
-
-export interface LinguistLanguage {
-  type: string
-  group: string
-  color: string
-  aliases: string[]
-  extensions: string[]
-  filenames: string[]
-  tm_scope: string
-  ace_mode: string
-  codemirror_mode: string
-  codemirror_mime_type: string
-  language_id: number
-}
 
 const EXTRA_SH_LANGUAGES: SupportLanguage[] = [
   {
@@ -52,89 +29,57 @@ const EXTRA_SH_LANGUAGES: SupportLanguage[] = [
   },
 ]
 
-const getSupportLanguages = (
-  languages: Record<string, LinguistLanguage>,
-  parser: 'sh' | 'sql',
-  aceModes: string[],
-) =>
-  Object.entries(languages).reduce<SupportLanguage[]>(
-    (acc, [name, language]) => {
-      const {
-        ace_mode: aceMode,
-        tm_scope: tmScope,
-        codemirror_mode: codemirrorMode,
-        codemirror_mime_type: codemirrorMimeType,
-        language_id: linguistLanguageId,
-      } = language
+const getSupportLanguages = (parser: 'sh' | 'sql', aceModes: string[]) =>
+  Object.values(LinguistLanguages).reduce<SupportLanguage[]>(
+    (
+      acc,
+      {
+        aceMode,
+        name,
+        type: _type,
+        color: _color,
+        languageId: linguistLanguageId,
+        ...rest
+      },
+    ) => {
       if (!aceModes.includes(aceMode)) {
         return acc
       }
       acc.push({
         name,
+        aceMode,
         since: '0.1.0',
         parsers: [parser],
-        ..._.pick(
-          language,
-          'group',
-          'aliases',
-          'extensions',
-          'filenames',
-          'interpreters',
-        ),
-        tmScope,
-        aceMode,
-        codemirrorMode,
-        codemirrorMimeType,
         linguistLanguageId,
         vscodeLanguageIds: [aceMode === 'sh' ? 'shellscript' : aceMode],
+        ...rest,
       })
       return acc
     },
     [],
   )
 
-get(
-  linguistLanguages,
-  {
-    agent: proxyUrl ? createHttpsProxyAgent(proxyUrl) : undefined,
-  },
-  res => {
-    let rawText = ''
-    res.on('data', (data: Buffer) => {
-      rawText += data.toString()
-    })
-    res.on('end', () => {
-      const allLanguages = load(rawText) as Record<string, LinguistLanguage>
-
-      fs.writeFileSync(
-        'packages/sh/src/languages.ts',
-        `import { SupportLanguage } from 'prettier'
+fs.writeFileSync(
+  'packages/sh/src/languages.ts',
+  `import { SupportLanguage } from 'prettier'
 
 export const languages = ${JSON.stringify(
-          [
-            ...getSupportLanguages(allLanguages, 'sh', [
-              'dockerfile',
-              'gitignore',
-              'properties',
-              'sh',
-            ]),
-            ...EXTRA_SH_LANGUAGES,
-          ],
-          null,
-          2,
-        )} as SupportLanguage[]`,
-      )
+    [
+      ...getSupportLanguages('sh', ['dockerfile', 'gitignore', 'sh']),
+      ...EXTRA_SH_LANGUAGES,
+    ],
+    null,
+    2,
+  )} as SupportLanguage[]`,
+)
 
-      fs.writeFileSync(
-        'packages/sql/src/languages.ts',
-        `import { SupportLanguage } from 'prettier'
+fs.writeFileSync(
+  'packages/sql/src/languages.ts',
+  `import { SupportLanguage } from 'prettier'
 
 export const languages = ${JSON.stringify(
-          [...getSupportLanguages(allLanguages, 'sql', ['sql', 'pgsql'])],
-          null,
-          2,
-        )} as SupportLanguage[]`,
-      )
-    })
-  },
+    [...getSupportLanguages('sql', ['sql', 'pgsql'])],
+    null,
+    2,
+  )} as SupportLanguage[]`,
 )
