7381a808c14a415abda07012582c76c90e445b79
JounQin <admin@1stg.me>
2022-09-06 22:10:07 +0800

    feat: use @huacnlee/autocorrect-node instead, use cosmiconfig to load config

--- packages/autocorrect/src/worker.ts
+++ packages/autocorrect/src/worker.ts
@@ -1,23 +1,20 @@
+import { formatFor, loadConfig } from '@huacnlee/autocorrect-node'
+import { cosmiconfig } from 'cosmiconfig'
 import { runAsWorker } from 'synckit'
 
 import type { FormatResult } from './types.js'
 
-let autocorrect: typeof import('@huacnlee/autocorrect/autocorrect.js')
-let loadedConfig: unknown
+const explorer = cosmiconfig('autocorrect')
 
 runAsWorker(async (source: string, filename: string) => {
-  autocorrect = await import('@huacnlee/autocorrect/autocorrect.js')
+  const result = await explorer.search(filename)
 
-  if (!loadedConfig) {
-    loadedConfig = autocorrect.loadConfig(
-      JSON.stringify({
-        spellcheck: {
-          mode: 1,
-          words: [],
-        },
-      }),
+  if (result) {
+    loadConfig(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      result.config, // type-coverage:ignore-line -- cosmiconfig's typings issue
     )
   }
 
-  return autocorrect.formatFor(source, filename) as Promise<FormatResult>
+  return formatFor(source, filename) as Promise<FormatResult>
 })
