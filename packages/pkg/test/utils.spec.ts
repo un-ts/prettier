bcfe8fa57b21f2ddd534e62c0d76698a3f0f0abe
Dom Porada <dom@dom.engineering>
2026-03-30 23:24:09 +0200

    refactor: tighten tests

--- packages/pkg/test/utils.spec.ts
+++ packages/pkg/test/utils.spec.ts
@@ -4,100 +4,148 @@ describe('sortScriptNames', () => {
   test('sorts pre/post hooks around base script names', () => {
     expect(
       sortScriptNames([
-        /* prettier-ignore */
         'format',
         'postformat',
         'posttest',
         'preformat',
         'pretest',
         'test',
+        'watch', // control
+        'patch', // control
+        'migrate', // control
+        'ignore', // control
+        'build', // control
       ]),
     ).toEqual([
-      /* prettier-ignore */
+      'build', // control
       'preformat',
       'format',
       'postformat',
+      'ignore', // control
+      'migrate', // control
+      'patch', // control
       'pretest',
       'test',
       'posttest',
-    ])
-
-    expect(
-      sortScriptNames([
-        /* prettier-ignore */
-        'test',
-        'postinstall',
-        'postpack',
-        'posttest',
-        'preinstall',
-        'prepack',
-        'pretest',
-      ]),
-    ).toEqual([
-      /* prettier-ignore */
-      'preinstall',
-      'postinstall',
-      'prepack',
-      'postpack',
-      'pretest',
-      'test',
-      'posttest',
+      'watch', // control
     ])
   })
 
   test('ignores unmatched pre/post hooks', () => {
     expect(
       sortScriptNames([
-        /* prettier-ignore */
-        'postformat',
+        'postformat', // (unmatched)
         'posttest',
-        'prelint',
+        'prelint', // (unmatched)
         'pretest',
         'test',
+        'watch', // control
+        'patch', // control
+        'migrate', // control
+        'ignore', // control
+        'build', // control
       ]),
     ).toEqual([
-      /* prettier-ignore */
-      'postformat',
-      'prelint',
+      'build', // control
+      'ignore', // control
+      'migrate', // control
+      'patch', // control
+      'postformat', // (unmatched)
+      'prelint', // (unmatched)
       'pretest',
       'test',
       'posttest',
+      'watch', // control
     ])
 
     expect(
       sortScriptNames([
-        /* prettier-ignore */
         'test',
         'pre',
         'post',
+        'watch', // control
+        'patch', // control
+        'migrate', // control
+        'ignore', // control
+        'build', // control
       ]),
     ).toEqual([
-      /* prettier-ignore */
+      'build', // control
+      'ignore', // control
+      'migrate', // control
+      'patch', // control
       'post',
       'pre',
       'test',
+      'watch', // control
     ])
   })
 
   test('respects default lifecycle scripts', () => {
+    // `pare` isn’t grouped with `prepare`
     expect(
       sortScriptNames([
-        /* prettier-ignore */
         'postprepare',
         'prepare',
         'preprepare',
         'test',
         'postpare',
         'pare',
+        'watch', // control
+        'patch', // control
+        'migrate', // control
+        'ignore', // control
+        'build', // control
       ]),
     ).toEqual([
-      /* prettier-ignore */
+      'build', // control
+      'ignore', // control
+      'migrate', // control
       'pare',
       'postpare',
+      'patch', // control
       'preprepare',
       'prepare',
       'postprepare',
       'test',
+      'watch', // control
+    ])
+
+    // DLS hooks are grouped together even though their base is missing
+    expect(
+      sortScriptNames([
+        'test',
+        'postversion', // (single DLS hook)
+        'postinstall',
+        'postmerge', // (not a DLS)
+        'postpack',
+        'posttest',
+        'preinstall',
+        'premerge', // (not a DLS)
+        'prepack',
+        'pretest',
+        'watch', // control
+        'patch', // control
+        'migrate', // control
+        'ignore', // control
+        'build', // control
+      ]),
+    ).toEqual([
+      'build', // control
+      'ignore', // control
+      'preinstall',
+      'postinstall',
+      'migrate', // control
+      'prepack',
+      'postpack',
+      'patch', // control
+      'postmerge', // (not a DLS)
+      'premerge', // (not a DLS)
+      'pretest',
+      'test',
+      'posttest',
+      'postversion', // (single DLS hook)
+      'watch', // control
     ])
   })
 })
